262fabbbabebc2d3383ff07af96e821ae50af9ed
AbhishekMondal1 <abhishekmondal.official@gmail.com>
2020-09-28 11:47:53 +0530

    null error fixed

--- src/App.js
+++ src/App.js
@@ -4,11 +4,20 @@ import Counters from "./components/counters";
 //import './App.css';
 
 class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ]
+  }
   handleIncrement = (counter) => {
     const counters = [...this.state.counters];
     const index = counters.indexOf(counter);
     counters[index] = { ...counter };
     counters[index].value++;
+    this.setState({ counters });
   };
 
   handleReset = () => {
@@ -18,6 +27,15 @@ class App extends Component {
     });
     this.setState({ counters });
   };
+
+  constructor(){
+      super();
+  }
+
+  componentDidMount(){
+      console.log('App - Mounter')
+  }
+
   handleDelete = (counterId) => {
     const counters = this.state.counters.filter((c) => c.id !== counterId);
     this.setState({ counters });
